cd63acb5f3c9d9a87496edd9e5250625cad1f277
Erwin van der Valk <erwin.vandervalk@duendesoftware.com>
2025-04-28 10:59:45 +0200

    Update access-token-management/src/AccessTokenManagement.OpenIdConnect/UserTokenManagementOptions.cs
    Co-authored-by: Joe DeCock <josephdecock@gmail.com>

--- access-token-management/src/AccessTokenManagement.OpenIdConnect/UserTokenManagementOptions.cs
+++ access-token-management/src/AccessTokenManagement.OpenIdConnect/UserTokenManagementOptions.cs
@@ -52,8 +52,8 @@ public class UserTokenManagementOptions
 
     /// <summary>
     /// By default, access token management uses <see cref="AccessTokenHandler"/>. Enabling this preview flag will replace this
-    /// with a more extensible model that allows you to use your own <see cref="ITokenRetriever{TToken}"/> and <see cref="ISendRequestRetryPolicy"/>
-    /// It switches to use <see cref="AccessTokenHandler{TTokenRetriever,TToken}"/>
+    /// with <see cref="AccessTokenHandler{TTokenRetriever,TToken}"/>, a more extensible model
+    /// that allows you to use your own <see cref="ITokenRetriever{TToken}"/> and <see cref="ISendRequestRetryPolicy"/>.
     /// 
     /// In future versions, this will be the default behavior.
     /// </summary>
