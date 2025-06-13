d15a75096d4b2cc3aa95c3b65394229d3339dc68
Joe DeCock <josephdecock@gmail.com>
2025-06-13 16:37:27 -0500

    Add support for client_secret_jwt

--- identity-model/src/IdentityModel/OidcConstants.cs
+++ identity-model/src/IdentityModel/OidcConstants.cs
@@ -382,6 +382,7 @@ public static class OidcConstants
         public const string PostBody = "client_secret_post";
         public const string BasicAuthentication = "client_secret_basic";
         public const string PrivateKeyJwt = "private_key_jwt";
+        public const string ClientSecretJwt = "client_secret_jwt";
         public const string TlsClientAuth = "tls_client_auth";
         public const string SelfSignedTlsClientAuth = "self_signed_tls_client_auth";
     }
