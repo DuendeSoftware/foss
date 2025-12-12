066617c97ab6c989921c91544d524466a8a0a226
Morten Løken <morten.loken@sr-bank.no>
2025-12-12 14:33:08 +0100

    Add public interface for ClientCredentialsCacheDurationStore

---
+++ access-token-management/src/AccessTokenManagement/IClientCredentialsCacheDurationStore.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Duende Software. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Duende.AccessTokenManagement;
+
+public interface IClientCredentialsCacheDurationStore
+{
+    TimeSpan GetExpiration(ClientCredentialsCacheKey cacheKey);
+    TimeSpan SetExpiration(ClientCredentialsCacheKey cacheKey, DateTimeOffset expiration);
+}
